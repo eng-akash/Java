41631f18b5ca2407464fd155a336b58eb27d5c91
eng-akash <131940138+eng-akash@users.noreply.github.com>
2024-01-15 23:15:25 +0530

    Update removeDuplicate.java

--- removeDuplicate.java
+++ removeDuplicate.java
@@ -4,7 +4,8 @@ public class removeDuplicate {
     public static int printNumber(int nums[]){
         int i=0;
         for (int j = 1; j<=nums.length-1; j++) {
-            if(nums[i]<nums[j]){
+            if(nums[i]<nums[j]){ //comparision
+                // swapping
                 int temp = nums[i+1];
                 nums[i+1]=nums[j];
                 nums[j]=temp;
@@ -14,6 +15,7 @@ public class removeDuplicate {
         }
         return i+1;
     }
+    // main function
     public static void main(String[] args) {
         int nums[] = {0,1,1,1,2,2,3,6,9};
         System.out.println(printNumber(nums));
