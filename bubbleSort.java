321e9b099ee2f9b1d7ca00495b04381c06bd262d
eng-akash <131940138+eng-akash@users.noreply.github.com>
2024-01-17 23:29:20 +0530

    Add files via upload

---
+++ bubbleSort.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class bubbleSort {
+    public static void getarray(int arr[]){
+        for (int i = 0; i <arr.length-1; i++) {
+            for (int j = 0; j <arr.length-1-i; j++) {
+                if(arr[j]>arr[j+1]){
+//                    swap
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+
+            }
+
+        }
+    }
+//    print the eliments
+    public static void printArr(int arr[]){
+        for (int i = 0; i<arr.length; i++) {
+            System.out.print(arr[i]+" ");
+
+        }
+        System.out.println();
+    }
+//    main function
+    public static void main(String[] args) {
+      int arr[] = {3,2,4,5,10};
+        getarray(arr);
+        printArr(arr);
+    }
+}
